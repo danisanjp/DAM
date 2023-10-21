69a33e137a366e01e6a5b8e3e827481474a4c955
danisanjp <146337705+danisanjp@users.noreply.github.com>
2023-10-21 18:50:43 +0200

    Create diferencias.sh

---
+++ EDD/diferencias.sh
@@ -0,0 +1,14 @@
+diff archivo1.txt archivo2.txt
+detecta cambios entre dos archivos
+
+a: added, añadido.
+c: changed, cambiado.
+d: deleted, eliminado.
+
+podemos usar el parametro -u en diff para ver de otra forma los cambios.
+
+explorar otras herramientas de cambios como:
+meld
+KDiff3
+vimdiff
+
